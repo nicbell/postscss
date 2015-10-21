c96ef645994fda2120fd870d04b40ed95e01e677
Nic Bell <nic@ixoxo.co.uk>
2015-10-21 18:53:50 +0100

    Write using promises.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* global Promise */
 var nodefs = require('node-fs');
 var path = require('path');
 var sass = require('node-sass');
@@ -10,54 +11,72 @@ function Processor(plugins) {
 	this.plugins = plugins;
 }
 
+
 /**
  * Process single
  */
 Processor.prototype.process = function (options) {
-	var fromName = path.basename(options.from),
-		toName = path.basename(options.to);
+	var plugins = this.plugins;
+	var toName = path.basename(options.to);
+	var dirName = path.dirname(options.to);
 
-	console.log(fromName + ' => ' + toName);
+	var sassPromise = new Promise(function (resolve, reject) {
+		console.log('Compiling SASS: ' + options.from);
 
-	sass.render({
-		file: options.from,
-		to: options.to,
-		outputStyle: 'compressed',
-		precision: 10,
-		sourceMap: true
-	}, function (err, result) {
-		console.log('SASS compiled.');
+		sass.render({
+			file: options.from,
+			to: options.to,
+			outputStyle: 'compressed',
+			precision: 10,
+			sourceMap: true
+		}, function (err, result) {
 
-		if (!err) {
-			//Post css
-			postcss(this.plugins).process(result.css, {
-				from: toName,
-				to: toName,
-				map: { inline: false }
-			}).then(function (result) {
+			if (err) {
+				reject(err);
+			}
+			else {
+				resolve(result);
+			}
+		});
+	});
 
-				var dirName = path.dirname(options.to);
+	var doPostCss = function (result) {
+		console.log('Running PostCss: ' + toName);
 
-				nodefs.mkdirSync(dirName, '0777', true);
-				nodefs.writeFile(options.to, result.css);
-				nodefs.writeFile(options.to + '.map', result.map);
+		//Post css already implements promises.
+		return postcss(plugins).process(result.css, {
+			from: toName,
+			to: toName,
+			map: { inline: false }
+		});
+	};
 
-				console.log('PostCSS transforms run.');
-			}.bind(this));
-		}
-		else {
-			console.log(err);
-		}
-	}.bind(this));
+	var writeToDisk = function (result) {
+		console.log('Writing file: ' + options.to);
+
+		nodefs.mkdirSync(dirName, '0777', true);
+		nodefs.writeFile(options.to, result.css);
+		nodefs.writeFile(options.to + '.map', result.map);
+	};
+	
+
+	return sassPromise
+		.then(doPostCss)
+		.then(writeToDisk);
 };
+
 
 /**
  * Process many
  */
 Processor.prototype.processMany = function (optionsArray) {
+	var proms = []
+
 	for (var i = 0; i < optionsArray.length; i++) {
-		this.process(optionsArray[i]);
+		proms.push(this.process(optionsArray[i]));
 	}
+
+	return Promise.all(proms);
 };
 
 
